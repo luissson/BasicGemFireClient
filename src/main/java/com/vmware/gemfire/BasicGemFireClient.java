01d360b3d2cd89306f72ccbfa2e6fe1075287663
Rene Jacome <jacomel@vmware.com>
2022-10-18 16:47:57 -0700

    fixups

--- src/main/java/com/vmware/gemfire/BasicGemFireClient.java
+++ src/main/java/com/vmware/gemfire/BasicGemFireClient.java
@@ -17,9 +17,9 @@ public class BasicGemFireClient {
     this.region = region;
   }
 
-  public static void main(String[] args) {
-    String locatorHostname = System.getenv("LOCATOR_HOST");
-    if (locatorHostname == null) {
+  public static void main(String[] args) throws InterruptedException {
+    String locatorAddress = System.getenv("LOCATOR_HOST");
+    if (locatorAddress == null) {
       System.out.println("Error: No environment value set for LOCATOR_HOST");
       System.exit(-1);
     }
@@ -33,15 +33,16 @@ public class BasicGemFireClient {
     Properties props = new Properties();
 
     props.setProperty("ssl-enabled-components", "all");
+    props.setProperty("ssl-endpoint-identification-enabled", "true");
     props.setProperty("ssl-keystore", "./certs/keystore.p12");
     props.setProperty("ssl-keystore-password", TRUST_PSWD);
     props.setProperty("ssl-truststore", "./certs/truststore.p12");
     props.setProperty("ssl-truststore-password", TRUST_PSWD);
 
     // connect to the locator using default port 10334
-    System.out.println("Attempting connection to locator " + locatorHostname);
+    System.out.println("Attempting connection to locator " + locatorAddress);
     ClientCache cache = new ClientCacheFactory(props)
-        .addPoolLocator(locatorHostname, 10334)
+        .addPoolLocator(locatorAddress, 10334)
         .set("log-level", "WARN").create();
 
     // create a local region that matches the server region
@@ -50,23 +51,25 @@ public class BasicGemFireClient {
             .create("example-region");
 
     BasicGemFireClient example = new BasicGemFireClient(region);
-    example.insertValues(10);
-    example.printValues(example.getValues());
+    example.putData(10);
+    example.getAndPrintValues(example.getRegionKeys());
 
     cache.close();
+
+    Thread.sleep(100000);
   }
 
-  Set<Integer> getValues() {
+  Set<Integer> getRegionKeys() {
     return new HashSet<>(region.keySetOnServer());
   }
 
-  void insertValues(int numValues) {
+  void putData(int numValues) {
     for(int i = 0; i < numValues; i++) {
       region.put(i, "value" + i);
     }
   }
 
-  void printValues(Set<Integer> values) {
+  void getAndPrintValues(Set<Integer> values) {
     for(Integer key : values) {
       System.out.printf("%d:%s%n", key, region.get(key));
     }
